45a5899115e92ef8072bcbd38b98fe7087d3ad38
sphoorthik <sphoorthi.k999@gmail.com>
2022-04-16 11:53:57 -0400

    Sample code for add art page

--- WebApp/frontend/crypto-gallery/src/App.js
+++ WebApp/frontend/crypto-gallery/src/App.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import './css/App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import AddArt from './components/AddArt';
 import AppNavbar from './components/AppNavbar';
 
 class App extends Component {
@@ -11,7 +12,7 @@ class App extends Component {
           <AppNavbar/>
           <Switch>
             <Route path='/' exact={true} component={Home}/>
-            <Route path='/art/new' component={Home}/>
+            <Route path='/art/new' component={AddArt}/>
             <Route path='/register' exact={true} component={Home}/>
           </Switch>
         </Router>
