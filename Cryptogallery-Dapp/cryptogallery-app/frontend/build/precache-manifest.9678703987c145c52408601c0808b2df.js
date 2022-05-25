b0e11ca195864b659a50afff258d9e9318a8b75a
ashish077 <ash.coolest7@gmail.com>
2022-05-24 23:20:00 -0400

    new changes

---
+++ Cryptogallery-Dapp/cryptogallery-app/frontend/build/precache-manifest.9678703987c145c52408601c0808b2df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "374043ba8f082ec4a580",
+    "url": "/static/css/main.fefa03fa.chunk.css"
+  },
+  {
+    "revision": "374043ba8f082ec4a580",
+    "url": "/static/js/main.5c0acc28.chunk.js"
+  },
+  {
+    "revision": "8cce959d96218c1fcc51",
+    "url": "/static/js/runtime~main.94570b12.js"
+  },
+  {
+    "revision": "426f5d3848cfe9f14d5c",
+    "url": "/static/css/2.58571510.chunk.css"
+  },
+  {
+    "revision": "426f5d3848cfe9f14d5c",
+    "url": "/static/js/2.198cb819.chunk.js"
+  },
+  {
+    "revision": "725329215980ccd5b30b",
+    "url": "/static/js/3.c6fc4e25.chunk.js"
+  },
+  {
+    "revision": "cdf74d4372a1f62306fcda8d3e3a59a2",
+    "url": "/index.html"
+  }
+];
